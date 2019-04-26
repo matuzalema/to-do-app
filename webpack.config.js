3615f785904aa726e6b28192e5e6c7e0bf40656c
matuzalema <matuzalema@gmail.com>
2019-04-26 10:14:23 +0200

    change task runner in package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,40 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    devtool: 'eval-source-map',
-    entry: './src/index.js',
-        output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'app.bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: "babel-loader"
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    { loader: 'style-loader'},
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: true
+module.exports = (env) => {
+    return {
+    // devtool: 'eval-source-map',
+        mode: env || 'production',
+        entry: './src/index.js',
+            output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: 'app.bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    loader: "babel-loader"
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        { loader: 'style-loader'},
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true
+                            }
                         }
-                    }
-                ]
-            }
-        ]
+                    ]
+                }
+            ]
 
-    }
+        },
+        plugins: [new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        })]
+    };
 };
